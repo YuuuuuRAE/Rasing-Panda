e361f003b7218d21300c651614edc9f6ba8ae7d0
YuRae <jennyr0406@dgu.ac.kr>
2023-11-24 18:05:42 +0900

    Update UI

--- RasingAPanda/Assets/Scripts/State.cs
+++ RasingAPanda/Assets/Scripts/State.cs
@@ -17,7 +17,12 @@ public class State : MonoBehaviour
     private TMP_Text affection_LV;                  //현재 레벨 텍스트
     [SerializeField]
     private TMP_Text coin;
-
+    [SerializeField,Header("얼굴 표정(청결도, 스트레스")]
+    private Sprite[] face;
+    [SerializeField]
+    private Image clean_face;
+    [SerializeField]
+    private Image stress_face;
 
     void Start()
     {
@@ -28,6 +33,58 @@ public class State : MonoBehaviour
     {
         updateAffection();
         updateLV();
+
+        updateStreeFace();
+        updateCleanFace();
+
+    }
+
+    private void updateStreeFace()
+    {
+        if(data.stress > 80)
+        {
+            stress_face.sprite = face[4];
+        }
+        else if(data.stress > 60)
+        {
+            stress_face.sprite = face[3];
+        }
+        else if(data.stress > 40)
+        {
+            stress_face.sprite = face[2];
+        }
+        else if(data.stress > 20)
+        {
+            stress_face.sprite = face[1];
+        }
+        else
+        {
+            stress_face.sprite = face[0];
+        }
+    }
+
+    private void updateCleanFace()
+    {
+        if(data.cleanliness > 80)
+        {
+            clean_face.sprite = face[0];
+        }
+        else if(data.cleanliness > 60)
+        {
+            clean_face.sprite = face[1];
+        }
+        else if(data.cleanliness > 40)
+        {
+            clean_face.sprite = face[2];
+        }
+        else if(data.cleanliness > 20)
+        {
+            clean_face.sprite = face[3];
+        }
+        else
+        {
+            clean_face.sprite = face[4];
+        }
     }
 
     private void updateAffection()
